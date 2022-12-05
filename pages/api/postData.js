1dacaef3514ca65fd145fa6b2bc6d6591daccb8a
Adrydevmateo <adrydevmateo@gmail.com>
2022-12-05 00:00:01 -0400

    ready

--- pages/api/postData.js
+++ pages/api/postData.js
@@ -1,6 +1,4 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-
-const arr = [];
+import { arr } from "./getData";
 
 export default function handler(req, res) {
   const body = req.body;
